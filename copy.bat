67dca2c50d755f4f26c9f465119162d159da9150
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-07 17:10:20 +0100

    copy.bat

--- copy.bat
+++ copy.bat
@@ -1,3 +1,5 @@
+robocopy out\artifacts ..\Team-C-SVM-Persistence\svm.persistence.implementation\lib
+
 robocopy out\artifacts ..\Team-C-SVM-Domain\svm.domain.implementation\lib
 
 robocopy out\artifacts ..\Team-C-SVM-Logic\svm.logic.implementation\lib
