b4cfc2515737fa913af33f6eba2c5eb122ea51c8
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-07 17:08:39 +0100

    Exception erstellt

---
+++ svm.exceptions/src/svm/persistence/abstraction/exceptions/NotSupportedException.java
@@ -0,0 +1,11 @@
+package svm.persistence.abstraction.exceptions;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+/**
+ * Projectteam : Team C
+ * Date: 07.11.12
+ */
+public class NotSupportedException extends Exception implements Remote,Serializable {
+}
